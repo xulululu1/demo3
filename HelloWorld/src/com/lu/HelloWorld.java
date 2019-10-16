f1ad18a8413201038066ea7407ed16523b5a3a0e
xulu_a <lgoodMorning_pro@atguigu.com>
2019-10-16 11:10:32 +0800

    新人修改

--- HelloWorld/src/com/lu/HelloWorld.java
+++ HelloWorld/src/com/lu/HelloWorld.java
@@ -3,5 +3,6 @@ package com.lu;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("GoodByeWorld!");
     }
 }
